3f2b8f854331832a54fb6bd148b13769f13a3c03
Jan Blackquill <uhhadd@gmail.com>
2022-01-08 14:59:23 -0500

    refactor: port to go 1.16 embed and templates

--- app/app.go
+++ app/app.go
@@ -1,18 +1,67 @@
 package app
 
 import (
+	"embed"
+	"errors"
+	"html/template"
+	"io"
+	"io/fs"
 	"log"
+	"net/http"
 
 	"github.com/harmony-development/inviter/app/config"
 	"github.com/harmony-development/inviter/app/manager"
-	"github.com/harmony-development/inviter/app/templateengine"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Template struct {
+	failed *template.Template
+	index  *template.Template
+	invite *template.Template
+}
+
+func New() *Template {
+	return &Template{
+		failed: template.Must(template.ParseFS(templates, "templates/failed.tmpl", "templates/application.tmpl")),
+		index:  template.Must(template.ParseFS(templates, "templates/index.tmpl", "templates/application.tmpl")),
+		invite: template.Must(template.ParseFS(templates, "templates/invite.tmpl", "templates/application.tmpl")),
+	}
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	switch name {
+	case "failed":
+		return t.failed.ExecuteTemplate(w, name+".tmpl", data)
+	case "index":
+		return t.index.ExecuteTemplate(w, name+".tmpl", data)
+	case "invite":
+		return t.invite.ExecuteTemplate(w, name+".tmpl", data)
+	default:
+		return errors.New("template not found")
+	}
+}
+
 type server struct {
 	Manager *manager.Manager
 	Config  *config.Config
+}
+
+//go:embed "templates"
+var templates embed.FS
+
+//go:embed "static"
+var static embed.FS
+
+var staticHTTP http.FileSystem
+
+func init() {
+	fsys, err := fs.Sub(static, "static")
+	if err != nil {
+		panic(err)
+	}
+
+	staticHTTP = http.FS(fsys)
 }
 
 // Start starts the app
@@ -25,12 +74,16 @@ func Start(m *manager.Manager) {
 	s := server{m, c}
 
 	e := echo.New()
-	e.Renderer = templateengine.Template{}
+	e.Renderer = New()
+	e.HTTPErrorHandler = func(e error, c echo.Context) {
+		println(e.Error())
+	}
 
 	e.Use(middleware.Recover())
 
-	e.Static("/static", "./app/static")
+	staticHandler := http.FileServer(staticHTTP)
 
+	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", staticHandler)))
 	e.GET("/_media/:homeserver/:attachment_id", s.attachmentProxy)
 	e.GET("/:homeserver/:invite_id", s.foreignInvite)
 	e.GET("/:invite_id", s.defaultInvite)
--- go.mod
+++ go.mod
@@ -1,27 +1,18 @@
 module github.com/harmony-development/inviter
 
-go 1.15
+go 1.16
 
 require (
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/creasty/defaults v1.5.2
-	github.com/golang/protobuf v1.5.2
-	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/gorilla/websocket v1.4.2
-	github.com/gosimple/slug v1.12.0 // indirect
-	github.com/harmony-development/shibshib v0.0.0-20220101224523-c98059d09cfa // indirect
+	github.com/harmony-development/shibshib v0.0.0-20220101224523-c98059d09cfa
 	github.com/hashicorp/golang-lru v0.5.4
 	github.com/hashicorp/hcl/v2 v2.11.1
-	github.com/jinzhu/gorm v1.9.16 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/labstack/echo/v4 v4.6.1
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/microcosm-cc/bluemonday v1.0.17 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
-	github.com/qor/qor v1.2.0 // indirect
-	github.com/qor/render v1.1.1
 	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/zclconf/go-cty v1.10.0 // indirect
 	github.com/ztrue/tracerr v0.3.0
@@ -29,6 +20,4 @@ require (
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/protobuf v1.27.1
 )
